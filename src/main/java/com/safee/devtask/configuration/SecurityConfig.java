fe6db6ae1eb084d64d55983219859152772d3837
Malaz Naoura <81930381+malaz-naoura@users.noreply.github.com>
2024-03-08 21:19:56 +0300

    Refactor: Replaced deprecated function calls with updated equivalents in SecurityConfig

--- src/main/java/com/safee/devtask/configuration/SecurityConfig.java
+++ src/main/java/com/safee/devtask/configuration/SecurityConfig.java
@@ -3,7 +3,10 @@ package com.safee.devtask.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
@@ -11,13 +14,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 
-        httpSecurity.csrf()
-                    .disable()
-                    .authorizeHttpRequests()
-                    .anyRequest()
-                    .authenticated()
-                    .and()
-                    .httpBasic();
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                    .authorizeHttpRequests(
+                            authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry.anyRequest()
+                                                                                                                    .authenticated())
+                    .httpBasic(withDefaults());
         return httpSecurity.build();
     }
 }
