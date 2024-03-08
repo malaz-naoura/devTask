d1372f7e0f8e14473eb07ea68e40ce0e2a1fd803
Malaz Naoura <81930381+malaz-naoura@users.noreply.github.com>
2024-03-08 21:07:49 +0300

    Fix: Uncomment some missing code in SecurityConfig.

--- src/main/java/com/safee/devtask/configuration/SecurityConfig.java
+++ src/main/java/com/safee/devtask/configuration/SecurityConfig.java
@@ -1,20 +1,23 @@
 package com.safee.devtask.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-//
-//        httpSecurity.csrf()
-//                    .disable()
-//                    .authorizeHttpRequests()
-//                    .anyRequest()
-//                    .authenticated()
-//                    .and()
-//                    .httpBasic();
-//        return httpSecurity.build();
-//    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.csrf()
+                    .disable()
+                    .authorizeHttpRequests()
+                    .anyRequest()
+                    .authenticated()
+                    .and()
+                    .httpBasic();
+        return httpSecurity.build();
+    }
 }
